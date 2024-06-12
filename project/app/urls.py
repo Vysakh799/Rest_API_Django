a88b6a5f267d3900666f9131e5de380174f8a9ef
Vysakh <vysakh7995@gmail.com>
2024-06-12 09:52:50 +0530

    new

--- project/app/urls.py
+++ project/app/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('',views.fun2),
     path('fun3',views.fun3),
     path('fun4/<d>',views.fun4),
+    path('fun5',views.fun5),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,9 +1,13 @@
+from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from .serializers import *
 from .models import *
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 def fun(request):
     return JsonResponse({'name':'Vysakh','age':12})
@@ -49,3 +53,17 @@ def fun4(request,d):
     elif request.method=='DELETE':
         demo.delete()
         return HttpResponse("Deleted")
+
+@api_view(['GET','POST'])
+def fun5(request):
+    if request.method=='GET':
+        d=student.objects.all()
+        s=model_serializers(d,many=True)
+        return Response(s.data)
+    elif request.method=='POST':
+        s=model_serializers(data=request.data)
+        if s.is_valid():
+            s.save()
+            return JsonResponse(s.data,status.HTTP_201_CREATED)
+        else:
+            return JsonResponse(s.errors,status.HTTP_400_BAD_REQUEST)
