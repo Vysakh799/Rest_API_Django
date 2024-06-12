71721827fd5b039316db5a217c0ad4f1c7fdd287
Vysakh <vysakh7995@gmail.com>
2024-06-12 09:12:31 +0530

    new

--- project/app/serializers.py
+++ project/app/serializers.py
@@ -5,3 +5,8 @@ class sample(serializers.Serializer):
     roll=serializers.IntegerField()
     name=serializers.CharField()
     place=serializers.CharField()
+
+class model_serializers(serializers.ModelSerializer):
+    class Meta:
+        model=student
+        fields='__all__'
--- project/app/urls.py
+++ project/app/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.fun2),
+    path('fun3',views.fun3),
+    path('fun4/<d>',views.fun4),
 ]
